49eaf858433ea3375b0c323218994154da0d700e
Sebastian Hoffmann <shoffmann.321@gmail.com>
2022-02-05 01:20:59 +0100

    :/

--- nem/logistic_regression.py
+++ nem/logistic_regression.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from absl import flags, app
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import LogisticRegression, SGDClassifier, LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.calibration import calibration_curve, CalibrationDisplay
 from sklearn.pipeline import make_pipeline
@@ -19,9 +20,9 @@ flags.DEFINE_string('track_file', 'songs_filtered.csv', 'the tracks file')
 RELEVANT_FEATURES = [
     'danceability',
     'energy',
-    'key',
+    #'key',
     'loudness',
-    'mode',
+    #'mode',
     'speechiness',
     'acousticness',
     'instrumentalness',  # extremly discriminative!
@@ -33,25 +34,33 @@ RELEVANT_FEATURES = [
 ]
 
 
-def train_and_evaluate_logistic(X, y, cv_folds = 5, train_size=0.8):
+def train_and_evaluate_logistic(X, y, popularity, cv_folds = 5, train_size=0.8):
+    indices = np.random.choice(len(X), len(X), replace=False)
+    indices_train = indices[:int(train_size*len(X))]
+    indices_test = indices[int(train_size*len(X)):]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size)
-    
+    X_train, y_train = X[indices_train], y[indices_train]
+    X_test, y_test = X[indices_test], y[indices_test]
+    pop_train, pop_test = (popularity)[indices_train], (popularity)[indices_test] 
+
     ## ML Pipeline
     pipe = make_pipeline(
-        StandardScaler(), 
-        GridSearchCV(
-            LogisticRegression(),
-            param_grid={
-            'penalty': ['l2'],
-            'class_weight': ['balanced'],
+        StandardScaler(),
+        #LinearRegression(),
+        MLPRegressor(verbose=True)
+
+        #GridSearchCV(
+        #    LogisticRegression(),   
+        #    param_grid={
+        #    'penalty': ['l2'],
+        #    'class_weight': ['balanced'],
             # 'l1_ratio': [0.5],
             # 'solver': ['saga'],
-            'C': np.logspace(-3, 3, 5)
-            },
-            cv=cv_folds,
-            refit=True
-        )
+        #    'C': np.logspace(-3, 3, 5)
+        #    },
+        #    cv=2,
+        #    refit=True
+        #)
     )
 
     # train
@@ -59,24 +68,53 @@ def train_and_evaluate_logistic(X, y, cv_folds = 5, train_size=0.8):
 
     # evaluate (i) ROC and AUC
     # calculate the fpr and tpr for all thresholds of the classification
-    probs = pipe.predict_proba(X_test)
-    preds = probs[:,1]
+    #probs = pipe.predict_proba(X_test)
+    #preds = probs[:,1]
+
+    #preds = 1 / (1 + np.exp(-pipe.predict(X_test)))
+    preds = np.exp(pipe.predict(X_test))
+    y_test = np.exp(y_test)
+
+    plt.figure()
+    plt.hist(preds, range=(0.0, 1.0))
+    plt.savefig('figures/pred_hist.pdf')
+    plt.close()
+
+    print(np.fabs(preds - y_test).mean())
+    print(np.power(preds - y_test, 2).mean())
+
+    """
     fpr, tpr, threshold = roc_curve(y_test, preds)
     roc_auc = auc(fpr, tpr)
 
     # evaluate (ii) metrics for logistic regression
     predictions = pipe.predict(X_test)
     report = classification_report(y_test, predictions)
+    """
 
     # evaluate (iii) calibration curve
-    y_prob = pipe.predict_proba(X_test)[:, 1]
-    (prob_true, prob_pred) = calibration_curve(y_test, y_prob, n_bins=5, strategy="uniform")
+    bins = np.quantile(y_test, np.linspace(0.01, 1., 15))
+
+    bin_grouping = np.digitize(y_test, bins)
+    prob_true = np.array([(y_test[bin_grouping == bin_idx]/100).mean() for bin_idx in range(len(bins))])
+    prob_pred = np.array([preds[bin_grouping == bin_idx].mean() for bin_idx in range(len(bins))])
+    print(bins)
+    print(prob_true)
+    print(prob_pred)
+    #y_prob = pipe.predict_proba(X_test)[:, 1]
+    #(prob_true, prob_pred) = calibration_curve((popularity/100)[indices_test], y_prob, n_bins=5, strategy="uniform")
     
 
     # bit of ugly...
     coefficients = np.ravel(pipe.steps[1][1].best_estimator_.coef_)
 
-    return (fpr, tpr, roc_auc), report, coefficients, (prob_true, prob_pred, y_prob)
+    fpr = 0.
+    tpr = 0.
+    roc_auc = 0.
+    report = None
+
+    return (fpr, tpr, roc_auc), report, coefficients, (prob_true, prob_pred, preds)
+
 
 def plot_coefs_barth(coefficients, model_name, filename):
     plt.rcParams.update(bundles.neurips2021())
@@ -114,7 +152,7 @@ def plot_calibration_curve(calibration_plots, filename):
 
     for cali_plot in calibration_plots:
         (prob_true, prob_pred, y_prob), name = cali_plot
-        plt.plot(prob_pred, prob_true, label=name, marker='o')
+        plt.plot(prob_true, prob_pred, label=name, marker='o')
 
     plt.xlim([-.1, 1.1])
     plt.ylim([-.1, 1.1])
@@ -163,17 +201,25 @@ def main(argv):
     X = tracks_relevant.values[:, :n_features]
     y = tracks_relevant.values[:, n_features]
 
+    X = X[(y>1) & (y < 95)]
+    y = y[(y>1) & (y < 95)]
+    y = np.log(y)
+
     y_labels_general =  np.where(y > np.quantile(y, 0.5), 1, 0)
     y_labels_tight =  np.where(y > np.quantile(y, 0.9), 1, 0)
 
-    auc_plot_general, evaluation_general, coeffs_general, calibration_plot_general = train_and_evaluate_logistic(X, y_labels_general)
-    auc_plot_tight, evaluation_tight, coeffs_tight, calibration_plot_tight = train_and_evaluate_logistic(X, y_labels_tight)
+    z = y/100
+    logit_labels =  np.log(z / (1-z))
+    auc_plot_general, evaluation_general, coeffs_general, calibration_plot_general = train_and_evaluate_logistic(X,y, y)
+    auc_plot_tight, evaluation_tight, coeffs_tight, calibration_plot_tight = train_and_evaluate_logistic(X, y/100, y)
 
+    """
     ## plot and save! 
     plot_auc_combined(auc_plot_general, auc_plot_tight, names=["50\% threshold model", "10\% threshold model"])
 
     plot_coefs_barth(coeffs_general, model_name="50\% threshold model", filename="50_threshold_model")
     plot_coefs_barth(coeffs_tight, model_name="weights 10\% threshold model", filename="10_threshold_model")
+    """
 
     plot_calibration_curve(
         (
